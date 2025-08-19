6cb5d805c995da3909c7de898e66d4584e87ca27
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2025-08-20 00:01:39 +1000

    Update __init__.py

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,16 +1,6 @@
 import logging
 import azure.functions as func
-import os
-from . import tracker
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Processing portfolio tracker function.')
-
-    try:
-
-        csv_path = os.path.join(os.path.dirname(__file__), "transactions.csv")
-        summary = tracker.get_portfolio_summary(csv_path=csv_path)
-        return func.HttpResponse(summary, mimetype="text/plain")
-    except Exception as e:
-        logging.exception("Unhandled exception in portfolio function:")
-        return func.HttpResponse(f"Error: {str(e)}", status_code=500)
+    logging.info("Function executed successfully.")
+    return func.HttpResponse("Function is alive.", status_code=200)
