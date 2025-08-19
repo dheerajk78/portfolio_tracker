37c6eeaa381d7575e4d5935ef1517e0ccc336c2f
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2025-08-19 23:31:02 +1000

    Update __init__.py

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,12 +1,15 @@
 import logging
 import azure.functions as func
+import os
 from . import tracker
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Processing portfolio tracker function.')
 
     try:
-        summary = tracker.get_portfolio_summary()
+
+        csv_path = os.path.join(os.path.dirname(__file__), "transactions.csv")
+        summary = tracker.get_portfolio_summary(csv_path=csv_path)
         return func.HttpResponse(summary, mimetype="text/plain")
     except Exception as e:
         logging.error(f"Error: {e}")
